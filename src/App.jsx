8071a8dc568b4506cc6438c42c7169acc6cca344
Roman Fedevych <fedevychr@gmail.com>
2024-03-28 23:40:20 +0200

    create and add comps to app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ContactList/ContactList.jsx
@@ -0,0 +1,11 @@
+import Contact from "../Contact/Contact";
+
+const ContactList = () => {
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
+};
+
+export default ContactList;
