c2ae3ab0013a0f94affa86de6225c67c77cf1b58
Roman Fedevych <fedevychr@gmail.com>
2024-03-30 02:22:02 +0200

    Add label, components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import ContactForm from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import SearchBox from "./SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
